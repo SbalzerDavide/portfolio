9914fbb20817c3595b618e0baa51b60d23b53cee
Davide <Sbalzerdavide@hotmail.it>
2023-07-12 13:27:42 +0200

    new novbar mobile

--- app.vue
+++ app.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <Navbar />
     <NuxtPage />
+    <navbarMobile />
+    <div class="cover"></div>
   </div>
 </template>
 
---
+++ components/NavbarMobile.vue
@@ -0,0 +1,47 @@
+<script setup>
+  const menu = useMenu();
+  const mobileMenu = ref(false);
+  function toggleMenuMobile() {
+    mobileMenu.value = !mobileMenu.value;
+    console.log(mobileMenu.value)
+  }
+</script>
+
+<template>
+  <nav class="navbar-mobile">
+    <div v-if="mobileMenu" class="mobile-blur"></div>
+    <i class="pi pi-bars" @click="toggleMenuMobile"></i>
+
+    <div 
+      class="navbar-right"
+      :class="mobileMenu ? 'navbar-right-mobile' : ''"
+    >
+      <div class="right-container">
+        <!-- <i class="pi pi-bars" @click="toggleMenuMobile"></i> -->
+        <ul 
+          class="menu"
+          :class="mobileMenu ? 'show-on-mobile' : ''"
+        >
+          <li 
+            v-for="(menuEl, index) in menu" 
+            :key="index"
+            :id="`navbar-menu-${menuEl.menuKey}`"
+          >
+          <a 
+            :href="`#${menuEl.title}`"
+            @click="toggleMenuMobile"
+          >
+            <div class="menu-key">{{ menuEl.menuKey }}.</div>
+            <div class="menu-desc">{{ menuEl.title }}</div>
+          </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+  @import '~/assets/scss/common.scss';
+  @import '~/assets/scss/navbar.scss';
+</style>
