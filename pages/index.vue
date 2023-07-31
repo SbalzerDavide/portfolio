b884bc1ebd141dec536580222b3319098c7d6894
Davide <Sbalzerdavide@hotmail.it>
2023-07-31 13:26:55 +0200

    add subtitle

--- pages/index.vue
+++ pages/index.vue
@@ -9,25 +9,28 @@
   <div class="container">
     <header>
       <h4 v-if="appConfig.language == 'en'">
-        Hi, my name is Davide Sbalzer
+        Hi, my name is Davide Sbalzer.
       </h4>
       <h4 v-else-if="appConfig.language == 'it'">
-        Ciao, mi chiamo Davide Sbalzer
+        Ciao, mi chiamo Davide Sbalzer.
       </h4>
 
       <h1 v-if="appConfig.language =='en'">
-        I build things for the web.
+        I build things for the web
+        <span class="flashing-underscore">_</span>
       </h1>
       <h1 v-else-if="appConfig.language == 'it'">
         Costruisco cose per il web
+        <span class="flashing-underscore">_</span>
       </h1>
 
-      <p v-else-if="appConfig.language == 'en'">
-        I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.
-      </p>
-      <p v-else-if="appConfig.language == 'it'">
-      </p>
-    </header>
+      <h3 v-if="appConfig.language == 'en'">
+        I’m a web developer specializing in building and designing digital experiences.
+      </h3>
+      <h3 v-else-if="appConfig.language == 'it'">
+        Sono uno web developer specializzato nello sviluppo e nella progettazione esperienze digitali.
+      </h3>
+      </header>
     <!-- :id="`box-${menuEl.menuKey}`" -->
     <Box
     v-for="(menuEl, index) in menu" 
@@ -55,6 +58,13 @@
     h1{
       color: $text-color-strong;
       font-size: 80px;
+      display: inline-block;
+    }
+    h3{
+      display: inline-block;
+      max-width: 600px;
+      font-weight: 400;
+      font-size: 18px;
     }
     h4{
       font-weight: 400;
@@ -66,9 +76,20 @@
       display: inline-block;
       width: 60%;
     }
+    .flashing-underscore{
+      animation: flash 1.4s linear infinite
+    }
   }
   .p-scrolltop{
     padding: 10px;
+  }
+  @keyframes flash {
+    0% {opacity: 0;}
+    1% {opacity: 1;}
+    50% {opacity: 1;}
+    51% {opacity: 0;}
+    99% {opacity: 0;}
+    100% {opacity: 1;}
   }
   @media screen and (max-width: 600px) {
     header{
